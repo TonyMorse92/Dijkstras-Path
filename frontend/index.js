d105322a63462a24ae36048b47cbf0025fc0cb0b
TonyMorse92 <anthonymorse92@gmail.com>
2025-06-22 18:23:31 -0400

    Have the cells clickable.

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,38 @@
-var request = new XMLHttpRequest();
-const url = "http://127.0.0.1:8000/items/5"
-request.open('GET', url, true);
-request.send();
+const grid = document.getElementsByClassName("container")[0];
 
-request.onload = function () 
+
+window.onload = function () 
 {
+
+	// Make each "cell" interactive
+	for (var i = 0; i < grid.children.length; i++) 
+	{
+		grid.children[i].onclick = function()
+		{	
+			//sendRequest(this);
+			test(this);
+		}
+	}	
+}
+
+function test(num)
+{	
+	alert(num.innerHTML);
+}
+
+function sendRequest(num)
+{
+	var request = new XMLHttpRequest();
+	const baseURL = "http://127.0.0.1:8000/fib/"
+	var URL = baseURL + num
+	request.open('GET', url, true);
+
 	// Begin accessing JSON data here
 	var data = JSON.parse(this.response);
 
 	if (request.status >= 200 && request.status < 400) 
 	{
-		alert(data.q);
+		alert(data.fib_n);
 	} 
 	else 
 	{
