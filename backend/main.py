632d6b93596001b67cd92b10e9876dcb299a2099
TonyMorse92 <anthonymorse92@gmail.com>
2025-06-22 17:20:45 -0400

    Have a simple request between frontend and backend.

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # Required for cross-origin requests
+
+
+app = FastAPI()
+
+# Enable CORS for local development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, adjust for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+	return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+	#return {"item_id": item_id, "q": q}
+	return {"item_id": item_id, "q": "Hello"}
--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,20 @@
-//alert("Hello, world");
+var request = new XMLHttpRequest();
+const url = "http://127.0.0.1:8000/items/5"
+request.open('GET', url, true);
+request.send();
 
-//[1,2].forEach(alert);
+request.onload = function () 
+{
+	// Begin accessing JSON data here
+	var data = JSON.parse(this.response);
+
+	if (request.status >= 200 && request.status < 400) 
+	{
+		alert(data.q);
+	} 
+	else 
+	{
+		const errorMessage = document.createElement('marquee');
+		errorMessage.textContent = `Gah, it's not working`;
+	}
+}
